8649789c2d9731de4d046fbb07d249a39c1a8bfb
IVEWOR <howdy@deepslog.com>
2024-06-24 23:28:43 +0530

    basic user routes

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+// getting the daa
+const users = require("./MOCK_DATA.json");
+
+// Middleware / plugin
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.get("/api/users", (req, res) => {
+  res.json(users);
 });
 
-app.get("/users/:id", (req, res) => {
-    const userId = req.params.id;
-    res.send(`user profile id: ${userId}`);
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+  res.send(html);
 });
 
-app.post("/submit", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    res.send(`form submitted successfully, name: ${name}, email: ${email}`);
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // edit user with id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // delete user with id
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: users.length });
+  });
 });
 
 const port = process.env.PORT || 5050;
 
 app.listen(port, () => {
-    console.log(`Server running at port: ${port}`);
+  console.log(`Server running at port: ${port}`);
 });
